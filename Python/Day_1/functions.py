48964840e376fc8eba58cd63cda242241c815d2e
Badokk <badokk@gmail.com>
2019-10-14 09:06:30 +0200

    Bit of an upgrade. Let's get back to work

--- Python/Day_1/functions.py
+++ Python/Day_1/functions.py
@@ -5,6 +5,53 @@ def sum_list(number_list):
 		sum = sum + int(number)
 	return sum
 	
+def prepare_partial_sums(number_list):
+	initial_partial_sums = list()
+	partial_sum = 0
+	for number in number_list:
+		initial_partial_sums.append(partial_sum)
+		partial_sum += int(number)
+	
+	total = partial_sum
+	return initial_partial_sums, total
+
+def prepare_congruent_groups(partial_sums, total):
+	grouped_partial_sums = dict()
+	for number in partial_sums:
+		remainder = number % total if total != 0 else number
+		if not remainder in grouped_partial_sums:
+			grouped_partial_sums[remainder] = list()
+		
+		grouped_partial_sums[remainder].append(number)
+	return grouped_partial_sums
+
+def find_candidates(grouped_partial_sums):
+	candidate_pairs = dict()
+	smallest_step = -1 # would be nice to get the first pair straight away
+	for key in grouped_partial_sums:
+		grouped_partial_sums[key].sort()
+		last_value = -1 # same
+		for value in grouped_partial_sums[key]:
+			if (last_value == -1):
+				last_value = value
+				continue
+			step = value - last_value
+			if (smallest_step == -1):
+				smallest_step = step
+			if (step < smallest_step):
+				candidate_pairs.clear()
+			if (step == smallest_step):
+				if not key in candidate_pairs:
+					candidate_pairs[key] = list()
+				candidate_pairs[key].append({last_value, value})
+			last_value = value
+	return candidate_pairs
+			# weird, it seems to be working. well, let's add more tests just in case
+
+			
+
+
+
 	
 def find_first_partial_sum_duplicate(number_list):
 	# # Brute force approach. Not very elegant
@@ -17,6 +64,41 @@ def find_first_partial_sum_duplicate(number_list):
 	# 		if (sum in previouslyFoundNumbers):
 	# 			return sum
 	# 		previouslyFoundNumbers.add(sum)
+	
+	### Prepare partial sums
+	(initial_partial_sums, total) = prepare_partial_sums(number_list)
+
+	# if total == 0:
+	# 	special case, need to sort the partial sums and check for duplicates
+	# 	return
+
+	### Prepare congruent groups
+	grouped_partial_sums = prepare_congruent_groups(initial_partial_sums, total)
+
+	# now the groups would likely need to be checked for duplicates... Or should
+	# they? the generic behaviour should point out duplicates quite easily
+	print(grouped_partial_sums)
+	for key in grouped_partial_sums:
+		print(key, grouped_partial_sums[key])
+
+	indices_to_delete = list()
+	for key in grouped_partial_sums: # i wonder if there's a kind of foreach that'd fit here
+		if (len(grouped_partial_sums[key]) < 2):
+			indices_to_delete.append(key)
+
+	for key in indices_to_delete:
+		del grouped_partial_sums[key]
+
+	print(grouped_partial_sums)
+	for key in grouped_partial_sums:
+		print(key, grouped_partial_sums[key])
+
+	# good. Now, there are two ways:
+	# 1. There are groups with more than one member -> find candidates fot title of first dupe
+	# 2. There are no such groups return false.
+
+	# candidates = findCandidates(groupedPartialSums)
+	
 
 	return (False, None)
 			
--- Python/Day_1/test.py
+++ Python/Day_1/test.py
@@ -8,15 +8,65 @@ def test_sum_list():
     assert functions.sum_list([0, 1, -1]) == 0
     assert functions.sum_list([0, 8, -4, 8]) == 12
 
-def test_find_duplicate():
-    assert functions.find_first_partial_sum_duplicate(
-        [0]) == (True, 0)
-    assert functions.find_first_partial_sum_duplicate(
-        [0, 1]) == (True, 1)
-    assert functions.find_first_partial_sum_duplicate(
-        [0, 1, -1]) == (True, 0)
-    assert functions.find_first_partial_sum_duplicate(
-        [0, 8, -4, 8]) == (True, 8)
+def prepare_partial_sums_util(list, expected_sum, expected_partial_sums):
+    partial_sums, sum = functions.prepare_partial_sums(list)
+    assert expected_partial_sums == partial_sums
+    assert expected_sum == sum
 
-    assert functions.find_first_partial_sum_duplicate(
-        [5, 10]) == (False, None)
+def test_prepare_partial_sums():
+    prepare_partial_sums_util(
+        [0],
+        0,
+        [0]
+    )
+
+    prepare_partial_sums_util(
+        [0, 1],
+        1,
+        [0, 0]
+    )
+
+    prepare_partial_sums_util(
+        [1, -1],
+        0,
+        [0, 1]
+    )
+    
+    prepare_partial_sums_util(
+        [8, -4, 8],
+        12,
+        [0, 8, 4]
+    )
+
+def test_prepare_congruent_groups():
+    res = functions.prepare_congruent_groups([1,2,3,4,5,6,7], 4)
+    assert res == {
+        0: [4],
+        1: [1, 5],
+        2: [2,6],
+        3: [3, 7]}
+
+    res = functions.prepare_congruent_groups([1,1,5], 4)
+    assert res == {
+        1: [1, 1, 5]}
+
+
+def test_find_candidates():
+    res = functions.find_candidates({0: [1,2], 1: [1,2]})
+    assert res == {
+        0: [{1,2}],
+        1: [{1,2}]
+    }
+    
+# def test_find_duplicate():
+#     assert functions.find_first_partial_sum_duplicate(
+#         [0]) == (True, 0)
+#     assert functions.find_first_partial_sum_duplicate(
+#         [0, 1]) == (True, 1)
+    # assert functions.find_first_partial_sum_duplicate(
+    #     [1, -1]) == (True, 0)
+#     assert functions.find_first_partial_sum_duplicate(
+#         [0, 8, -4, 8]) == (True, 8)
+
+    # assert functions.find_first_partial_sum_duplicate(
+    #     [5, 10]) == (False, None)
